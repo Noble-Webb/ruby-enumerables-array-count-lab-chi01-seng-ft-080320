5f784f8c925fee3be57285b0db77ca3851dd9b05
Justin Webb <webb.n.justin@gmail.com>
2020-07-20 19:11:02 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -8,7 +8,7 @@ end
 def count_empty_strings(array)
   find = ""
   array.count do |empty|
-    empty.class == find.class
+    empty == find
   end 
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
